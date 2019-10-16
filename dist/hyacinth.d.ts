84b800c546e59f577b6389e1ed25323ae9748314
KeisukeYamashita <19yamashita15@gmail.com>
2019-10-16 12:25:24 +0900

    :up: upgrade to 1.0.5

--- dist/hyacinth.d.ts
+++ dist/hyacinth.d.ts
@@ -1,1 +1,11 @@
+import { StarSign, StarSigns } from './starSign';
+declare class Hyacinth {
+    private date;
+    constructor(d?: any);
+    private getDatetime;
+    private isDayjs;
+    starSign(): StarSign;
+    list(): StarSigns;
+}
+declare function hyacinth(d?: any): Hyacinth;
 export default hyacinth;
