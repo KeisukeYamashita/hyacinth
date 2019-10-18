abf701156459af26556c622b1da323406b2e13b3
KeisukeYamashita <19yamashita15@gmail.com>
2019-10-18 09:30:52 +0900

    :dress: upgrade index to common ones

--- src/starsign.ts
+++ src/starsign.ts
@@ -26,18 +26,18 @@ export interface StarSign {
 
 const starSigns: StarSigns = {
     signs: [
-    { id: 1, name: 'やぎ座', borderValue: 119 },
-    { id: 2, name: 'みずがめ座', borderValue: 218 },
-    { id: 3, name: 'うお座', borderValue: 320 },
-    { id: 4, name: 'おひつじ座', borderValue: 419 },
-    { id: 5, name: 'おうし座', borderValue: 520 },
-    { id: 6, name: 'ふたご座', borderValue: 621 },
-    { id: 7, name: 'かに座', borderValue: 722 },
-    { id: 8, name: 'しし座', borderValue: 822 },
-    { id: 9, name: 'おとめ座', borderValue: 922 },
-    { id: 10, name: 'てんびん座', borderValue: 1023 },
-    { id: 11, name: 'さそり座', borderValue: 1122 },
-    { id: 12, name: 'いて座', borderValue: 1221 }
+    { id: 10, name: 'やぎ座', borderValue: 119 },
+    { id: 11, name: 'みずがめ座', borderValue: 218 },
+    { id: 12, name: 'うお座', borderValue: 320 },
+    { id: 1, name: 'おひつじ座', borderValue: 419 },
+    { id: 2, name: 'おうし座', borderValue: 520 },
+    { id: 3, name: 'ふたご座', borderValue: 621 },
+    { id: 4, name: 'かに座', borderValue: 722 },
+    { id: 5, name: 'しし座', borderValue: 822 },
+    { id: 6, name: 'おとめ座', borderValue: 922 },
+    { id: 7, name: 'てんびん座', borderValue: 1023 },
+    { id: 8, name: 'さそり座', borderValue: 1122 },
+    { id: 9, name: 'いて座', borderValue: 1221 }
 ]};
 
 
